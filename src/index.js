e10f9497f08ecc456354aa6b19e5c41d974669a7
nghiaphunng <duynghia135z@gmail.com>
2025-02-22 22:30:12 +0700

    learn router: setup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <h2>Login Page</h2>,
+  },
+  {
+    path: "/register",
+    element: <h2>Register Page</h2>,
+  },
+  {
+    path: "/users",
+    element: <h2>User List Page</h2>,
+  },
+  {
+    path: "/products",
+    element: <h2>Product List Page</h2>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
